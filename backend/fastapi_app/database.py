fd502ff940df6851bca5a055e4e66628ee6005d6
winsixed <i@winsixed.ru>
2025-07-05 00:46:26 +0300

    push

--- backend/fastapi_app/database.py
+++ backend/fastapi_app/database.py
@@ -1,13 +1,15 @@
+from dotenv import load_dotenv
+load_dotenv()
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-DATABASE_URL = 'sqlite:///./fastapi_app/app.db'
+# Connection string provided via environment variable
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False} if DATABASE_URL.startswith('sqlite') else {}
-)
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
